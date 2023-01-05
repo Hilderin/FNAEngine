fe962ae416e707c2ce7f5dcc925fa1818dfca34f
Hilderin <81109165+Hilderin@users.noreply.github.com>
2023-01-05 12:32:21 -0500

    Moving ColliderSample

---
+++ FNAEngine2D.ColliderSample/Ball.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FNAEngine2D.ColliderSample
+{
+    public class Ball: GameObject
+    {
+
+        private GameObject _obj;
+        private Vector2 _movement = Vector2.Zero;
+
+        public Ball()
+        {
+            this.Width = 4;
+            this.Height = 4;
+        }
+
+        protected override void Load()
+        {
+            _obj = Add(new TextureRender("circle", this.Bounds));
+            //this.EnableCollider();
+            this.AddComponent(new ColliderCircle(new Vector2(this.Width / 2), this.Width / 2));
+            _movement = new Vector2(GameMath.RandomFloat(-1, 1), GameMath.RandomFloat(-1, 1));
+        }
+
+
+        protected override void Update()
+        {
+            Vector2 delta = _movement * 3 * this.ElapsedGameTimeSeconds * this.NbPixelPerMeter;
+            Vector2 nextLocation = _obj.Location + delta;
+
+            Collision collision = this.GetCollision(nextLocation, null);
+            if (collision != null)
+            {
+                if (collision.Direction == CollisionDirection.MovingColliderOnTop || collision.Direction == CollisionDirection.MovingColliderOnBottom)
+                    _movement.Y *= -1;
+                else
+                    _movement.X *= -1;
+
+                foreach (GameObject obj in collision.CollidesWith)
+                {
+                    if (obj is Ball)
+                    {
+                        if (collision.Direction == CollisionDirection.MovingColliderOnTop || collision.Direction == CollisionDirection.MovingColliderOnBottom)
+                            ((Ball)obj)._movement.Y *= -1;
+                        else
+                            ((Ball)obj)._movement.X *= -1;
+                    }
+                }
+
+                delta = _movement * 3 * this.ElapsedGameTimeSeconds * this.NbPixelPerMeter;
+                nextLocation = _obj.Location + delta;
+            }
+            
+                this.Location = nextLocation;
+        }
+    }
+}
---
+++ FNAEngine2D.ColliderSample/Sample.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FNAEngine2D.ColliderSample
+{
+    public class Sample: GameObject
+    {
+        /// <summary>
+        /// Load
+        /// </summary>
+        protected override void Load()
+        {
+            //Top
+            Add(new TextureRender("pixel", new Rectangle(0, 0, this.Game.Width, 20), Color.White)).EnableCollider();
+            //Right
+            Add(new TextureRender("pixel", new Rectangle(this.Game.Width - 20, 0, 20, this.Game.Height), Color.White)).EnableCollider();
+            //Bottom
+            Add(new TextureRender("pixel", new Rectangle(0, this.Game.Height - 20, this.Game.Width, 20), Color.White)).EnableCollider();
+            //Left
+            Add(new TextureRender("pixel", new Rectangle(0, 0, 20, this.Game.Height), Color.White)).EnableCollider();
+
+            for(int cpt = 0; cpt < 1000; cpt++)
+                Add(new Ball()).TranslateTo(GameMath.RandomFloat(25, this.Game.Width - 25 - 50), GameMath.RandomFloat(25, this.Game.Height - 25 - 50));
+
+            Add(new FPSRender(ContentManager.FONT_ROBOTO_REGULAR, 12, Color.DarkRed));
+        }
+
+    }
+}
